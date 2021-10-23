b63ad638a1803573b1e7d3d0928b6a1553059166
🎃 Christoffer 🎃 <strand.arnesen@gmail.com>
2021-10-23 17:04:17 +0200

    Laste opp annonser til DB, mangler tilbud/etterspørsel knapper

--- app/src/main/java/com/simpliest/giveget/NyAnnonse_Fragment.kt
+++ app/src/main/java/com/simpliest/giveget/NyAnnonse_Fragment.kt
@@ -6,17 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 
 class NyAnnonse_fragment : Fragment() {
 
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
     }
 
@@ -33,6 +38,48 @@ class NyAnnonse_fragment : Fragment() {
             transaction.replace(R.id.secondLayout,secondFragment)
             transaction.commit()
         }
+
+        val publiserBtn = v.findViewById<Button>(R.id.publiserbtn)
+        val sendTitle = v.findViewById<EditText>(R.id.titleField)
+        val sendDesc = v.findViewById<EditText>(R.id.descField)
+
+
+
+
+        //listener for "Publiser annonse" knappen
+        publiserBtn.setOnClickListener {
+            //Konstruktør for annonsene
+            data class Annonse(val tittel: String, val beskrivelse: String, val id: String)
+
+            //henter og gjør om tittel og beskrivelse til String
+            val title = sendTitle.text.toString()
+            val desc = sendDesc.text.toString()
+            //genererer en random ID, brukes som "Key" til den enkelte annonsen
+            val unikID = UUID.randomUUID().toString()
+
+            //Referanse til der Annonsen skal lagres
+            database = FirebaseDatabase.getInstance().getReference("AnnonseAndroid")
+
+                //Lager et annonse objekt ved bruk av konstruktøren
+                val annonse = Annonse(title, desc, unikID)
+
+                database.child(unikID).setValue(annonse).addOnSuccessListener {
+                        sendTitle.text.clear()
+                        sendDesc.text.clear()
+
+                    }.addOnFailureListener {
+                        //Gjør dette, dersom det feiler
+                        Toast.makeText(
+                            this.context,
+                            "Kunne ikke opprette annonse",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+            }
+
+
+
         return v
 
     }
